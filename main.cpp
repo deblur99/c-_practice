922a6b3b365398e60dabd5d1487e72fecc45abc1
deblur99 <deblur99@kakao.com>
2021-07-26 20:14:48 +0900

    added-7-26

--- main.cpp
+++ main.cpp
@@ -12,11 +12,9 @@ public:
     Power(int kick = 0, int punch = 0)
         : kick(kick), punch(punch) {};
     void show();
-    friend Power operator + (int op1, Power op2); // 외부 함수를 friend 함수로써 초대
-    Power operator + (int n);
-    Power& operator ++ ();
-    bool operator ! ();
-    Power& operator -- (int x);
+    friend Power& operator ++ (Power & op);
+    friend Power& operator ++ (Power & op, int x);
+    Power& operator << (int x);
 };
 
 void Power::show() {
@@ -24,39 +22,32 @@ void Power::show() {
     return;
 }
 
-Power operator + (int op1, Power op2) {
-    Power ret;
-    ret.kick = op1 + op2.kick;
-    ret.punch = op1 + op2.punch;
-    return ret;
+Power& operator ++ (Power & op) {
+    op.punch++;
+    op.kick++;
+    return op;
 }
 
-Power Power::operator+(int n) {
-    Power ret;
-    ret.kick = kick + n;
-    ret.punch = punch + n;
-    return ret;
+Power& operator ++ (Power & op, int x) {
+    op.punch++;
+    op.kick++;
+    return op;
 }
 
-Power& Power::operator ++ () {
-    kick++; punch++;
-    return *this; // 객체의 this 포인터의 역참조를 반환
-}
-
-bool Power::operator ! () {
-    return kick == 0 && punch == 0;
-}
-
-Power& Power::operator -- (int x) {
-    kick--;
-    punch--;
+Power& Power::operator << (int x) {
+    kick += x;
+    punch += x;
     return *this;
 }
 
 int main() {
     Power a(3, 5);
-    Power b = 2 + a;
-    b.show();
+
+    a.show();
+
+    a << 5 << 8 << 6;
+
+    a.show();
 
     return 0;
 }
